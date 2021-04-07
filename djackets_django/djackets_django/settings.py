77a4e3d99e6d7c358d378a2aee340ef6c2c3fdeb
Hikmat Samadov <hikmetsemedov0177@gmail.com>
2021-04-07 19:03:25 +0400

    Product App

--- djackets_django/djackets_django/settings.py
+++ djackets_django/djackets_django/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'djoser',
     # Created
+    'product',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -131,6 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
